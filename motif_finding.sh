3c8c1fa99df44522af912ae80e434087b5e8736e
Aarav Mehta <122249133+aaravmehta7@users.noreply.github.com>
2024-12-08 23:55:15 -0500

    Update motif_finding.sh

--- motif_finding.sh
+++ motif_finding.sh
@@ -1,4 +1,4 @@
-
+sort -k5nr BRCA_peakCalls.txt > sorted_peaks.txt
 head -n 50000 sorted_peaks.txt > top_peaks.txt
 awk '{print $1"\t"$2"\t"$3"\t"$4}' top_50K_peaks.txt > top_50K_peaks.bed
 meme top_50K_peaks.fa -dna -mod zoops -nmotifs 20 -o top_50K_motifs
